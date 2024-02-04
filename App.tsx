22c43f448473e7890802873c796b58c524c6ce64
JeffersonD4vid <Jeffersonmd2002@gmail.com>
2024-02-04 10:31:37 -0500

    add

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaView } from "react-native"
+import { Inicio } from './src/screem/Inicio';
+
+const App = () =>{
+  return(
+    <SafeAreaView>
+      <Inicio/>
+    </SafeAreaView>
+  )
+}
+
+export default App;
